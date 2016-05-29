4eb70c7a7a5a234f18316bec56f793af3f0dc8b5
kbon <bon.kevin@gmail.com>
2016-05-28 23:27:10 -0400

    Added: Mine colorisation (Cyan: You, Violet: Enemy)

--- battlestick-helper.js
+++ battlestick-helper.js
@@ -10,16 +10,31 @@
 unsafeWindow.console.log('battlestick-helper: Activated');
 var config = {
   mine: {
+    enemyMineIdentifier: {
+      active: true,
+      myColor: 0x00ffff, // Cyan
+      enemyColor: 0xff00ff, // Violet
+    },
     enemyMineNeverHide: true
   }
 };
-/*
-  --- Mine ---
-  Fn: - Enemy mines never hide
-*/
+// Helper
+var isMyStickMan = function() {
+  return null !== myStickman && this.stickman === myStickman;
+}
+
+ // --- Mine ---
+// Fn: - Enemy mines never hide
 if (config.mine.enemyMineNeverHide) {
   unsafeWindow.Mine.prototype.update = function () {
     if (null !== myStickman && this.stickman !== myStickman && myStickman.life > 0) {
     } else null !== myStickman && myStickman.life <= 0 && (this.sprite.alpha = 1)
+  }
+}
+if (config.mine.enemyMineIdentifier.active) {
+  unsafeWindow.Mine.prototype._create = unsafeWindow.Mine.prototype.create;
+  unsafeWindow.Mine.prototype.create = function() {
+    this._create();
+    this.sprite.tint = isMyStickMan.call(this) ? config.mine.enemyMineIdentifier.myColor : config.mine.enemyMineIdentifier.enemyColor;;
   }
 }
